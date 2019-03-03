a7920bd1cb47ea774d8feec96d510d454677b66f
Marek Felsoci <marek.felsoci@gmail.com>
2019-03-03 11:00:23 +0100

    Reimplemented table of symbols

---
+++ include/tos.h
@@ -0,0 +1,20 @@
+#ifndef TOS_H
+#define TOS_H
+
+// Everytime a new symbol is about to be inserted into the table of symbols, the size of the table will be increased by TOS_GROW_SIZE if there is no more space to insert the new symbol.
+#define TOS_GROW_SIZE 10
+
+#include "symbol.h"
+
+// Definition of symbol list type
+typedef struct {
+  Symbol ** data;
+  size_t size,
+         index;
+} TOS;
+
+TOS * tos_alloc(void);
+TOS * tos_append(TOS *, Symbol *);
+void tos_free(TOS *);
+
+#endif // TOS_H
---
+++ src/tos.c
@@ -0,0 +1,49 @@
+#include "tos.h"
+
+TOS * tos_alloc(void) {
+  TOS * table = (TOS *) malloc(sizeof(TOS));
+  if(!table) {
+    return NULL;
+  }
+
+  table->data = (Symbol **) malloc(TOS_GROW_SIZE * sizeof(Symbol *));
+  if(!table->data) {
+    free(table);
+    return NULL;
+  }
+
+  table->size = TOS_GROW_SIZE;
+  table->index = 0;
+
+  return table;
+}
+
+TOS * tos_append(TOS * table, Symbol * symbol) {
+  if(!table || !symbol) {
+    return NULL;
+  }
+
+  if(table->index >= table->size) {
+    table->data = (Symbol **) realloc(table->data, table->size + TOS_GROW_SIZE);
+    if(!table->data) {
+      return NULL;
+    }
+  }
+
+  table->data[table->index++] = symbol;
+
+  return table;
+}
+
+void tos_free(TOS * table) {
+  if(!table) {
+    return;
+  }
+
+  for(size_t i = 0; i < table->index; i++) {
+    sy_free(table->data[i]);
+  }
+
+  free(table->data);
+  free(table);
+}
